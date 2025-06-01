ddd870b886029c17e70032b9172f7341e486045b
fancys0cks <ghizmoblume@gmail.com>
2025-06-01 20:54:42 +0200

    Added Items

--- src/main/java/net/fancy_socks/electricum/Electricum.java
+++ src/main/java/net/fancy_socks/electricum/Electricum.java
@@ -3,6 +3,7 @@ package net.fancy_socks.electricum;
 import net.fabricmc.api.ModInitializer;
 
 import net.fancy_socks.electricum.block.ModBlocks;
+import net.fancy_socks.electricum.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,5 +15,7 @@ public class Electricum implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModBlocks.registerBlocks();
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/fancy_socks/electricum/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.fancy_socks.electricum.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fancy_socks.electricum.Electricum;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item LITHIUM = registerItem("lithium", new Item(new Item.Settings()));
+    public static final Item RAW_LITHIUM = registerItem("raw_lithium", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Electricum.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Electricum.LOGGER.info("Registering Mod Items for " + Electricum.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(LITHIUM);
+            fabricItemGroupEntries.add(RAW_LITHIUM);
+        });
+    }
+}
