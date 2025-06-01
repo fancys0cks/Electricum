db6268ea54807630fbafbed70cf6dbc7bd65caad
fancys0cks <ghizmoblume@gmail.com>
2025-06-01 10:30:43 +0200

    Initial commit

---
+++ src/main/java/net/fancy_socks/electricum/Electricum.java
@@ -0,0 +1,17 @@
+package net.fancy_socks.electricum;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Electricum implements ModInitializer {
+	public static final String MOD_ID = "electricum";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fancy_socks/electricum/electricumClient.java
@@ -0,0 +1,10 @@
+package net.fancy_socks.electricum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class electricumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
